a2c44059a5733cb99f46a1b7bc52e35f8e50e940
笨木头 <musicvs@163.com>
2020-07-07 14:23:04 +0800

    Create publish.sh

---
+++ jenkins/scripts/publish.sh
@@ -0,0 +1,15 @@
+cd C:/Mutou/JenkinsPublish
+
+$compressedName="dist.7z";
+$cmdargs = "7z a "+$compressedName+" -w .\*"
+cmd /c $cmdargs
+
+$publishDir="C:/Mutou/JenkinsPublish/webProject"
+if(Test-Path $publishDir)
+{
+    Remove-Item $publishDir -Recurse -Force
+}
+
+md $publishDir
+
+Copy-Item compressedName -Destination $publishDir
